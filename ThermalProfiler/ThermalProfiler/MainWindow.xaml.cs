a220977e831905a8c8c674d8042fd673afacbfc9
onishiryoya <onishi@hapis.k.u-tokyo.ac.jp>
2021-12-23 14:49:54 +0900

    実験に使ったもの

--- ThermalProfiler/ThermalProfiler/MainWindow.xaml.cs
+++ ThermalProfiler/ThermalProfiler/MainWindow.xaml.cs
@@ -153,7 +153,7 @@ namespace ThermalProfiler
         {
             AUTD autd = new AUTD();
             autd.AddDevice(Vector3d.Zero, Vector3d.Zero);
-            //autd.AddDevice(new Vector3d(0, 0, 0), Vector3d.Zero);
+            autd.AddDevice(new Vector3d(0, 0, 0), Vector3d.Zero);
             //autd.AddDevice(Vector3d.Zero, Vector3d.Zero);
             //autd.AddDevice(new Vector3d(0, 206.4, 0), Vector3d.Zero);
             //autd.AddDevice(new Vector3d(250, 0, 0), Vector3d.Zero);
@@ -212,7 +212,7 @@ namespace ThermalProfiler
 
             int frameNum = 0;
 
-            var directoryName = @"D:\onishi_Local2/Nature/exp_for_paper/SigleFocusRotateDuty50/degree0/" + DateTime.Now.ToString("yyyy_MM_dd_HH_mm");
+            var directoryName = @"D:\onishi_Local2/Nature/exp_for_paper/StandingWaveRotateDuty50/degree90/" + DateTime.Now.ToString("yyyy_MM_dd_HH_mm");
 
             int z_change = 170;
             float y_change = 151.4f;
